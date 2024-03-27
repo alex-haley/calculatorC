e746f048847992ae1bec2442419977aff0be2209
keymoki <mloyygraduate7@gmail.com>
2024-03-27 09:16:08 +0800

    fix issue with square operator and issue with advanced options menu

--- main_op.c
+++ main_op.c
@@ -13,6 +13,7 @@ int main(void)
 {
   // в самом начале назначаю две переменные для их счета
   int a, b;
+  char val2;
 
   // приветствие и выбор оператора калькулятора
   // advance options in development
@@ -24,6 +25,7 @@ int main(void)
   scanf("%c", &val);
 
   printf("\n");
+
   // функция если val = +
   if (val == '+')
   {
@@ -50,21 +52,16 @@ int main(void)
   // (advanced options)
   else if (val == 'a')
   {
-    char val2;
-    printf("advanced options:\n"
-           "- derivative (d)\n"
-           "- multiplication (m)\n"
-           "- square (s)\n"
-           "what option to choose? (or get back to first menu pressing 'c'): ");
-    scanf("%c", &val2);
-
+    printf("advance: ");
+    scanf(" %c", &val2);
     // derivative
     if (val2 == 'd')
     {
       printf("(d)enter two digits separate: ");
       scanf("%d %d", &a, &b);
 
-      der_op(a, b);
+      der_op(a,b);
+
       return 0;
     }
     
@@ -75,6 +72,7 @@ int main(void)
       scanf("%d %d", &a, &b);
 
       mul_op(a, b);
+
       return 0;
     }
 
@@ -85,7 +83,10 @@ int main(void)
       scanf("%d %d", &a, &b);
 
       square_op(a, b);
+
+      return 0;
     }
+    return 0;
   }
 
   else
@@ -94,7 +95,6 @@ int main(void)
     return 1;
   }
 
-  return 0;
 }
 
 // определение функции сложения
@@ -103,7 +103,7 @@ int plus_op(int a, int b)
   int sum;
 
   sum = a + b;
-  printf("your answer is: %d\n", sum);
+  printf("answer is: %d\n", sum);
 }
 
 // определение функции вычитания
@@ -111,30 +111,33 @@ int minus_op(int a, int b)
 {
   // выбор положения цифр в вычитании
   int choice, mis;
-  printf("a - b or b - a? (1, 2):\n");
+  printf("a - b or b - a? (1, 2): ");
   scanf("%d", &choice);
+  printf("\n");
 
   if (choice == 1)
   {
     mis = a - b;
 
-    printf("your answer is: %d\n", mis);
+    printf("answer is: %d\n", mis);
   }
   else
   {
     mis = b - a;
 
-    printf("your answer is: %d\n", mis);
+    printf("answer is: %d\n", mis);
   }
 }
 
 // declare derivative operator
 int der_op(int a, int b)
 {
-  int der;
+  float der;
 
-  der = a / b;
-  printf("answer is: %f\n", der);
+  der = (float)a/b;
+  printf("answer is: %.2f\n", der);
+
+  return 0;
 }
 
 // declare multiplication operator
@@ -151,6 +154,6 @@ int square_op(int a, int b)
 {
   int sq;
 
-  sq = pow(double(a), double(b));
+  sq = pow(a, b);
   printf("answer is: %d\n", sq);
 }
